b2abb907f585ead7289e8565ea9c7c41cb74ae51
ismayilhsnv <tu7u02d2s@code.edu.az>
2023-10-12 13:41:05 +0400

    off

---
+++ script.js
@@ -0,0 +1,8 @@
+// let string='Salam necesiz';
+// let newString='';
+// for(let i=o;i<string.length-1;i++){
+//     if (string[i] !==''){
+//         newString += string[i]
+//     }
+// }
+// console.log(newString);
